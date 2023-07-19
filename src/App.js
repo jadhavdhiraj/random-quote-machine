0d243aa0e682bb59279bd3f60fa04cf169bdb364
DHIRAJ JADHAV <jadhavdhiraj29m@gmail.com>
2023-07-19 16:17:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [quoteInfo,setQuoteInfo] = useState({})
+  useEffect(() =>{
+    getQuote();
+
+},[]);
+
+async function getQuote(){
+  const response = await fetch('https://api.quotable.io/random')
+  const quote = await response.json()
+  setQuoteInfo({
+    text:quote.content,
+    author:quote.author
+  })
+}
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="quote-box">
+        <p id="text">{quoteInfo.text}</p>
+        <p id="author">{quoteInfo.author}</p>
+        <button id="new-quote" className="buttons" onClick={getQuote}>New Quote</button>
+        <a id="tweet-quote" href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text='+quoteInfo.text}>Twitter</a>
+      </div>
     </div>
   );
 }
